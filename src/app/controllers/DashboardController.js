e896ba82b07b59a485ab8d144772a603a46ea51d
Davi <davirodrigues8@gmail.com>
2019-02-25 00:48:57 -0300

    section for the service provider to follow the day's schedules

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,9 +1,47 @@
-const { User } = require('../models')
+const moment = require('moment')
+const { Op } = require('sequelize')
+const { User, Appointment } = require('../models')
 
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
-    return res.render('dashboard', { providers })
+    const { user } = req.session
+    if (!user.provider) {
+      const subtitle =
+        'Inicie um agendamento escolhendo um profissional abaixo: '
+      const providers = await User.findAll({ where: { provider: true } })
+
+      return res.render('dashboard', { providers, subtitle })
+    } else {
+      const subtitle = 'Agendamentos do dia: '
+      // Selecionando agendamentos do dia
+      const appointments = await Appointment.findAll({
+        where: {
+          provider_id: user.id,
+          date: {
+            [Op.between]: [
+              moment()
+                .startOf('day')
+                .format(),
+              moment()
+                .endOf('day')
+                .format()
+            ]
+          }
+        },
+        order: [['date']]
+      })
+
+      // Salvando horários com usuarios
+      const schedules = await Promise.all(
+        appointments.map(async a => {
+          return {
+            horary: moment(a.date).format('HH:mm'),
+            user: await User.findByPk(a.user_id)
+          }
+        })
+      )
+      return res.render('dashboard', { schedules, subtitle })
+    }
   }
 }
 
